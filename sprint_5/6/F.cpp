1e654c954cd5df4485203d07db291c7d3a69037a
Anastasia Shilova <bunnnyyy@mail.ru>
2025-01-14 19:52:59 +0300

    sprint_5_task_F

---
+++ sprint_5/6/F.cpp
@@ -0,0 +1,45 @@
+#ifndef REMOTE_JUDGE
+struct Node {  
+  int value;  
+  const Node* left = nullptr;  
+  const Node* right = nullptr;
+  Node(int value, Node* left, Node* right) : value(value), left(left), right(right) {}
+};  
+#endif
+
+#ifdef REMOTE_JUDGE
+#include "solution_tree.h"
+#endif
+#include <cmath>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int dfs(const Node* root) {
+	if(root == nullptr) {
+    	return 0;
+    }
+    
+    return std::max(dfs(root->left), dfs(root->right)) + 1;
+}
+
+int Solution(const Node* root) {
+	return dfs(root);
+} 
+
+#ifndef REMOTE_JUDGE
+void test() {
+    Node node1({1, nullptr, nullptr});
+    Node node2({4, nullptr, nullptr});
+    Node node3({3, &node1, &node2});
+    Node node4({8, nullptr, nullptr});
+    Node node5({5, &node3, &node4});
+    assert(Solution(&node5) == 3);
+}
+
+
+int main() {
+  test();
+}
+#endif
